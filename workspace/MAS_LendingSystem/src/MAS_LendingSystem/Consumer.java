d30a28c01cbb369adcdfa5dab12484db027826e6
agentjwall <agentjwall@gmail.com>
2015-04-13 19:29:24 -0500

    reduced vars to what is still valid

--- workspace/MAS_LendingSystem/src/MAS_LendingSystem/Consumer.java
+++ workspace/MAS_LendingSystem/src/MAS_LendingSystem/Consumer.java
@@ -8,9 +8,10 @@ public class Consumer {
 	static int costOfLiving = 1000;  // 1000/ month = 12k annually 
 	
 	int income = 0; //Income gained per tick
-	double spending; //Money spent per tick
+	double spending = 0; //Money spent per tick
 	double desiredLoanAmount; //Money wanted from loan in the current tick
-	double desiredSpending;
+	int splurgeThreshold = 0;
+	int splurgeDesire = 0;
 	int risk = 0; //0-1 percent risk of defaulting	
 	double netWorth = 0; 
 	ArrayList<Loan> loans = new ArrayList<Loan>(); //Loans currently held by agent
@@ -20,39 +21,21 @@ public class Consumer {
 		//this.income = Dist(Consumer.costOfLiving, 20000)
 	}
 	
-	public void step() {
-		double neighborsAvg = receiveNeighborsAvgSpending(); //TODO implement communication  
-		this.updateDesiredSpending(neighborsAvg);
-		double moneySpent = this.spendMoney();
-		this.netWorth = this.netWorth + this.income - moneySpent + (moneySpent * Consumer.percentDurable);
+	public void step() {  
+		this.netWorth = this.netWorth + this.income - this.spending + (this.spending * Consumer.percentDurable);
 	}
 	
 	// returns amount of money spent
-	private double spendMoney() {
-		double actualSpent;
-		if (this.desiredSpending < income) {
-			actualSpent = this.desiredSpending;
-		} else {
-			actualSpent = Consumer.costOfLiving + ((this.income - Consumer.costOfLiving) * this.risk);
-		}
-		return actualSpent;
-	}
 	
-	private void updateDesiredSpending(double neighbors_avg) {
-		int r;
-		if (this.desiredSpending - neighbors_avg < this.desiredSpending) {
-			r = 1 - this.risk;
-		} else {
-			r = this.risk;
-		}
-		this.desiredSpending = this.desiredSpending + (r * (neighbors_avg - this.desiredSpending)); 
+	private void updateSplurgeDesire(int neighborsSplurge) {
+		
 	}
 	
 	//TODO implement
-	public double receiveNeighborsAvgSpending() {
+	public double[] receiveNeighborsAvgSplurging() {
 		// ask all neighbors what they are spending
 		// return the average of that
-		return 0;
+		return null;
 	}
 	
 	public double getNetWorth() {
