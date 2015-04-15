29da9f8a0a0a0793415b0a969ec528964f55e5ba
agentjwall <agentjwall@gmail.com>
2015-04-14 19:36:19 -0500

    added support for 1.7.0_75

--- workspace/MAS_LendingSystem/src/MAS_LendingSystem/RoundDownConverter.java
+++ workspace/MAS_LendingSystem/src/MAS_LendingSystem/RoundDownConverter.java
@@ -2,14 +2,14 @@ package MAS_LendingSystem;
 import repast.simphony.parameter.*;
 public class RoundDownConverter implements StringConverter<Object> {
 
-	@Override
+	
 	public String toString(Object obj) {
 		return obj.toString();
 	}
 
-	@Override
+	
 	public Object fromString(String strRep) {
-		int inputVal = (int) new StringConverterFactory.IntConverter().fromString(strRep);
+		int inputVal = (Integer) new StringConverterFactory.IntConverter().fromString(strRep);
 		if (inputVal % 2 != 0) {
 			inputVal--;
 		}
