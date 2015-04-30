ba4fef1d8799f5228f5f45e9d5985a455ede0013
Cassey Lottman <clottman@live.com>
2015-04-29 23:18:48 -0500

    added analysis code

---
+++ analysis/make_graphs.R
@@ -0,0 +1,46 @@
+setwd("/Users//clottman//Dropbox//Junior Year//MAS//project");
+
+e1_assets <- read.csv("Assets Output E1 600a.csv")
+e2_assets <- read.csv("Assets Output E2 600a.csv")
+e1_tpc <- read.csv("TPC Output E1 600agents.csv")
+e2_tpc <- read.csv("TPC Output E2 600a.csv")
+
+## helper function section
+
+# get all the unique values in the run column
+getRunNumbers <- function(data) {
+  unique(unlist(data$run, use.names = FALSE))
+}
+
+# gets the indexes of the rows containing data for a given run
+# example: getRunNumbers(2) will return all the indexes where the run column contains 2
+getIndexesByRun <- function(list, desiredRun) {
+  which(list$run == desiredRun)
+}
+
+                      
+## execution section
+runNumbers <- getRunNumbers(e1_tpc)
+
+for (run in runNumbers) {
+  indexes <- getIndexesByRun(e1_assets, run)
+  tpc <- e1_tpc$total.percent.change[indexes]
+  tpc_name <- paste("tpc", run, sep="_")
+  run_name <- paste("run", run, sep="_")
+  e1_tpc[[tpc_name]] <- tpc
+}
+
+## Graph 1, 5, 9, 2, 6, 10
+plot(e1_tpc$tpc_1,type="l",col=1, xlim=c(0, 240), xlab="Tick", ylab="Total % Change in Economy")
+lines(e1_tpc_force, col=2)
+lines(e1_tpc$tpc_5,col=2)
+lines(e1_tpc$tpc_9, col=3)
+lines(e1_tpc$tpc_2, col=4)
+lines(e1_tpc$tpc_6, col=5)
+lines(e1_tpc$tpc_12, col=2)
+
+## Graph 1, 5, and 9
+plot(e1_tpc$tpc_1,type="l",col=1, xlim=c(0, 240), xlab="Tick", ylab="Total % Change in Economy")
+lines(e1_tpc$tpc_5,col=2)
+lines(e1_tpc$tpc_9, col=4)
+
